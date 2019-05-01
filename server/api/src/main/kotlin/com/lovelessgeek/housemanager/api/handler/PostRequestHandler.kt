cbcf0391d21598844c8dfcfd72f9c048639c5b7c
nayoonho <soldier4443@gmail.com>
2019-05-01 19:16:01 +0900

    Post Handler error handling

--- server/api/src/main/kotlin/com/lovelessgeek/housemanager/api/handler/PostRequestHandler.kt
+++ server/api/src/main/kotlin/com/lovelessgeek/housemanager/api/handler/PostRequestHandler.kt
@@ -4,8 +4,11 @@ import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
+import com.github.pgutkowski.kgraphql.ExecutionException
+import com.github.pgutkowski.kgraphql.RequestException
 import com.lovelessgeek.housemanager.api.fromJson
 import com.lovelessgeek.housemanager.api.request.PostRequestParams
+import com.lovelessgeek.housemanager.api.toJson
 import com.lovelessgeek.housemanager.graphql.schema
 
 class PostRequestHandler :
@@ -17,10 +20,26 @@ class PostRequestHandler :
     ): APIGatewayProxyResponseEvent {
         println("Input: ${input?.body}")
         val body = input?.body?.fromJson<PostRequestParams>()
-            ?: return error(422, "body is missing in POST request!")
+            ?: return error(400, "body is missing in POST request!")
 
         val query = body.query
+        val variables = body.variables.toJson()
 
-        return body(schema.execute(query))
+        return try {
+            body(schema.execute(query, variables))
+        } catch (e: Exception) {
+            when (e) {
+                is RequestException,
+                is ExecutionException,
+                is IllegalArgumentException -> {
+                    System.err.println(e.message)
+                    error(400, e.message ?: "")
+                }
+                else -> {
+                    e.printStackTrace()
+                    error(500, e.message ?: "")
+                }
+            }
+        }
     }
 }
