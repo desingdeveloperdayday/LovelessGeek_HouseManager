f9d86af0a7583d33b5fc28c089f4e3726bc37fde
nayoonho <soldier4443@gmail.com>
2019-05-13 23:35:46 +0900

    [Android] Fix: Make repository singleton

--- android/app/src/main/java/com/lovelessgeek/housemanager/data/Provider.kt
+++ android/app/src/main/java/com/lovelessgeek/housemanager/data/Provider.kt
@@ -4,7 +4,13 @@ import android.content.Context
 import com.lovelessgeek.housemanager.data.db.LocalDatabase
 
 object Provider {
+    private var repository: Repository? = null
+
     fun provideRepository(context: Context): Repository {
-        return RepositoryImpl(LocalDatabase.getInstance(context))
+        return repository ?: makeRepository(context)
+            .also { repository = it }
     }
+
+    private fun makeRepository(context: Context) =
+        RepositoryImpl(LocalDatabase.getInstance(context.applicationContext))
 }
