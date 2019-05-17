d57e2974eaa7cb7758a9e91b5308fa7ba6164494
nayoonho <soldier4443@gmail.com>
2019-05-18 01:50:27 +0900

    [Android] Add: 모두 BaseActivity 기반으로 변경

--- android/app/src/main/java/com/lovelessgeek/housemanager/ui/splash/SplashActivity.kt
+++ android/app/src/main/java/com/lovelessgeek/housemanager/ui/splash/SplashActivity.kt
@@ -2,11 +2,11 @@ package com.lovelessgeek.housemanager.ui.splash
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import com.lovelessgeek.housemanager.R
+import com.lovelessgeek.housemanager.base.BaseActivity
 import com.lovelessgeek.housemanager.ui.main.MainActivity
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
